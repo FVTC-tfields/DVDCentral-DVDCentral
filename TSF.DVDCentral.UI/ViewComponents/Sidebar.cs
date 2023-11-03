1b2fdc2c620e0131235928969c181cde72e93c0a
Tyler Fields <300032214@fvtc.edu>
2023-11-02 21:17:34 -0500

    fixed all link

--- TSF.DVDCentral.UI/ViewComponents/Sidebar.cs
+++ TSF.DVDCentral.UI/ViewComponents/Sidebar.cs
@@ -6,22 +6,22 @@ namespace TSF.DVDCentral.UI.ViewComponents
 {
     public class Sidebar : ViewComponent
     {
-        public IViewComponentResult Invoke()
-        {
-            var data = GenreManager.Load().OrderBy(p => p.Description).ToList();
-            var allGenre = new BL.Models.Genre()
-            {
-                Id = -1,
-                Description = "All"
-            };
-            data.Add(allGenre);
-
-            return View(data);
-        }
-
         //public IViewComponentResult Invoke()
         //{
-        //    return View(GenreManager.Load().OrderBy(p => p.Description));
+        //    var data = GenreManager.Load().OrderBy(p => p.Description).ToList();
+        //    var allGenre = new BL.Models.Genre()
+        //    {
+        //        Id = -1,
+        //        Description = "All"
+        //    };
+        //    data.Add(allGenre);
+
+        //    return View(data);
         //}
+
+        public IViewComponentResult Invoke()
+        {
+            return View(GenreManager.Load().OrderBy(p => p.Description));
+        }
     }
 }
